7391dd537049bab679e0bc8ad33599cf46fb8a41
madspe <madspe@uia.no>
2022-01-28 14:47:07 +0100

    kasmdlmkf

--- event/event_test.go
+++ event/event_test.go
@@ -5,10 +5,10 @@ package event
 
 import "testing"
 
-func TestPut(t *testing.T) {
+func Put(t *testing.T) {
     // Hva forventer jeg?
     wanted := "[kylling rev korn ---\\ \\_korn_/ _________________/---]"
-    got := Put("korn") // Hva fikk jeg?
+    got := Put("rev") // Hva fikk jeg?
     if got != wanted {
              t.Errorf("Feil, fikk %q, ønsket %q.", got, wanted)
     }
